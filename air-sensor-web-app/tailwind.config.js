9d520bd769c3bcd79afb47e8ccaa5824d410361b
CoachhCal <calvinj.murray@hotmail.com>
2025-08-10 15:19:01 -0300

    cleaned up more code, deleted unused pictures

--- air-sensor-web-app/tailwind.config.js
+++ air-sensor-web-app/tailwind.config.js
@@ -1,8 +1,7 @@
-// tailwind.config.js
 module.exports = {
   content: [
-    './index.html',                // Make sure Tailwind processes your HTML
-    './src/**/*.{vue,js,ts,jsx,tsx}', // Make sure Tailwind processes your Vue components
+    './index.html',                
+    './src/**/*.{vue,js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
